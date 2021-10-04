082961e7a4a810558b5e997c668c632ceb31f7ba
Will <will_lowe1@outlook.com>
2021-10-04 18:49:17 +0100

    tweak front end

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, UserInRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view())
 ]
